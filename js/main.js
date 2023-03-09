0856e62f2a866be5c30bff29855530b58cb95908
Alisher <abduvaliyevalisher003@gmail.com>
2023-03-09 11:57:41 +0500

    add style

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let elBoxLeftBtn = document.querySelector(".box-bacground-left__btn");
+let elBoxRightBtn = document.querySelector(".box-bacground-right__btn");
+let elBoxAuto = document.querySelector(".box-auto");
+let elboxAutoSignUp = document.querySelector(".box-auto-SignUp");
+let elboxAutoLogin = document.querySelector(".box-auto-login");
+
+elboxAutoLogin.style.display = "none";
+
+
+elBoxLeftBtn.addEventListener("click", function() {
+  elBoxAuto.classList.add("box-auto-right")
+  elBoxAuto.classList.remove("box-auto-left")
+  elboxAutoSignUp.style.display = "inline"
+  elboxAutoLogin.style.display = "none";
+})
+
+elBoxRightBtn.addEventListener("click", function() {
+  elBoxAuto.classList.add("box-auto-left")
+  elBoxAuto.classList.remove("box-auto-right")
+  elboxAutoSignUp.style.display = "none"
+  elboxAutoLogin.style.display = "inline";
+})
+
