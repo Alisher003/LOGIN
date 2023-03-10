a0c61fca86f7bc472cffdb8701ce8a66017d5c96
Alisher <abduvaliyevalisher003@gmail.com>
2023-03-10 15:25:51 +0500

    add responsive style

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let elBoxRightBtn = document.querySelector(".box-bacground-right__btn");
 let elBoxAuto = document.querySelector(".box-auto");
 let elboxAutoSignUp = document.querySelector(".box-auto-SignUp");
 let elboxAutoLogin = document.querySelector(".box-auto-login");
+let elBtnLogin = document.querySelector(".btn-login");
+let elBtnSignUp = document.querySelector(".btn-SignUp");
 
 elboxAutoLogin.style.display = "none";
 
@@ -20,3 +22,21 @@ elBoxRightBtn.addEventListener("click", function() {
   elboxAutoSignUp.style.display = "none"
   elboxAutoLogin.style.display = "inline";
 })
+
+// RESPONSIVE 
+
+elBtnLogin.addEventListener("click", function() {
+  elBoxAuto.classList.add("box-auto-left")
+  elBoxAuto.classList.remove("box-auto-right")
+  elboxAutoSignUp.style.display = "none"
+  elboxAutoLogin.style.display = "inline";
+})
+
+elBtnSignUp.addEventListener("click", function() {
+  elBoxAuto.classList.add("box-auto-left")
+  elBoxAuto.classList.remove("box-auto-right")
+  elboxAutoSignUp.style.display = "inline"
+  elboxAutoLogin.style.display = "none";
+})
+
+
